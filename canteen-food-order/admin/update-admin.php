29b76270380aec484a2ffedc794c331ad6da501f
wayne puah <wayne_p@greypanel.com>
2024-03-22 16:01:59 +0800

    debug if else condition

--- canteen-food-order/admin/update-admin.php
+++ canteen-food-order/admin/update-admin.php
@@ -5,42 +5,36 @@
         <h1>Update Admin</h1>
 
         <br><br>
- 
-        <?php 
-            
-            //1. Get the ID of selected Admin
-            $id = isset($_GET['id']) ? $_GET['id'] : '';
 
-            //2. Create SQL Query to GET the Details
-            $sql="SELECT * FROM tbl_admin WHERE id=$id";
+        <?php
 
-            //Execute the Query
-            $res = mysqli_query($conn,$sql);
+        //1. Get the ID of selected Admin
+        $id = isset($_GET['id']) ? $_GET['id'] : '';
 
-            //Check wheter the query is executed or not 
-            if($res==true)
-            {
-                //Check wheter the Data is available or not
-                $count = mysqli_num_rows($res);
-                //Check whether we have admin data or not
-                if($count==1)
-                {
-                    //Get the Details
-                    //echo "Admin Available";
-                    $row=mysqli_fetch_assoc($res);
+        //2. Create SQL Query to GET the Details
+        $sql = "SELECT * FROM tbl_admin WHERE id=$id";
 
-                    $full_name = $row['full_name'];
-                    $username = $row['username'];
-                }
-            
-                else
-                {
-                    //Redirect to Manage Admin Page
-                    header('location:'.SITEURL.'admin/manage-admin.php');
-                }
+        //Execute the Query
+        $res = mysqli_query($conn, $sql);
+
+        //Check wheter the query is executed or not 
+        if ($res == true) {
+            //Check wheter the Data is available or not
+            $count = mysqli_num_rows($res);
+            //Check whether we have admin data or not
+            if ($count == 1) {
+                //Get the Details
+                //echo "Admin Available";
+                $row = mysqli_fetch_assoc($res);
+
+                $full_name = $row['full_name'];
+                $username = $row['username'];
+            } else {
+                //Redirect to Manage Admin Page
+                header('location:' . SITEURL . 'admin/manage-admin.php');
             }
-        
-        ?>  
+        }
+        ?>
 
         <form action="" method="POST">
 
@@ -57,7 +51,7 @@
                     <td>
                         <input type="text" name="username" value="<?php echo $username; ?>">
                     </td>
-                </tr>>
+                </tr>
 
                 <tr>
                     <td colspan="2">
@@ -72,46 +66,41 @@
     </div>
 </div>
 
-<?php 
+<?php
+//Check whether the submit button is Clicked or not
+if (isset($_POST['submit'])) {
+    // echo "Button Clicked";
+    //Get all the values from form to update
+    $id = $_POST['id'];
+    $full_name = $_POST['full_name'];
+    $username = $_POST['username'];
 
-    //Check whether the submit button is Clicked or not
-    if(isset($_POST['submit']));
-    {
-        //echo "Button Clicked";
-        //Get all the values from form to update
-        $id = $_POST['id'];
-        $full_name = $_POST['full_name'];
-        $username = $_POST['username'];
+    //Create a SQL Query to update Admin
+    $sql = "UPDATE tbl_admin SET
+    full_name = '$full_name',
+    username = '$username'
+    WHERE id='$id'
+    ";
 
-        //Create a SQL Query to update Admin
-        $sql = "UPDATE tbl_admin SET
-        full_name = '$full_name',
-        username = '$username'
-        WHERE id='$id'
-        ";
-      
 
-        // //Execite the Query
-        // $res = mysqli_query($conn, $sql);
+    // //Execite the Query
+    // $res = mysqli_query($conn, $sql);
 
-         //Check whethwe the query executed successfully or not
-         if($res==true)
-         {
-             //Query executed and Admin Updated
-             $_SESSION['update'] = "<div class='success'>Admin Updated Successfully.</div>";
-             //Redirect to Manage Admin Page
-             header('location:'.SITEURL.'admin/manage-admin.php');
-         }
-         else
-         {
-             //Failed to Update Admin
-             $_SESSION['update'] = "<div class='error'>Failed to Delete Admin.</div>";
-             //Redirect to Manage Admin Page
-            header('location:'.SITEURL.'admin/manage-admin.php');
-         }
+    //Check whethwe the query executed successfully or not
+    if ($res == true) {
+        //Query executed and Admin Updated
+        $_SESSION['update'] = "<div class='success'>Admin Updated Successfully.</div>";
+        //Redirect to Manage Admin Page
+        header('location:' . SITEURL . 'admin/manage-admin.php');
+    } else {
+        //Failed to Update Admin
+        $_SESSION['update'] = "<div class='error'>Failed to Delete Admin.</div>";
+        //Redirect to Manage Admin Page
+        header('location:' . SITEURL . 'admin/manage-admin.php');
     }
+}
 
 
-?>   
+?>
 
 <?php include('partials/footer.php'); ?>
